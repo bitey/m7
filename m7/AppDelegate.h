103b71579dba6cac19a47750ac67e204239f0dc7
bitey <bitey@outlook.com>
2013-11-29 20:39:03 -0500

    new project

---
+++ m7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  m7
+//
+//  Created by Bitey on 11/29/13.
+//  Copyright (c) 2013 Bagel Bites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ m7/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  m7
+//
+//  Created by Bitey on 11/29/13.
+//  Copyright (c) 2013 Bagel Bites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
